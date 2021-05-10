ac98d4eeb3ec1f3329e34b444e15a95d689afd49
Matias Borghi <borghi.matias@gmail.com>
2021-05-10 10:40:20 -0300

    changed page description

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: 'London',
-  shortName: 'London',
-  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
-  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
+  name: 'Projects',
+  shortName: 'Projects',
+  description: 'Welcome! Please, take a look at all the projects I have been doing',
+  url: 'https://mattborghi.github.io/projects', // Domain of your site without prefix!
   prefix: '/',
-  author: 'Imed Adel',
-  twitter: 'Imed_Adel'
+  author: 'Matias Borghi',
+  twitter: 'Matias Borghi'
 }
