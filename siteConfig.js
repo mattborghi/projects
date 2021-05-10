6f87cc795dfc2fd545c8c5eb6117f7a7acfd9f45
Matias Borghi <borghi.matias@gmail.com>
2021-05-10 13:15:33 -0300

    add deployment lines

--- siteConfig.js
+++ siteConfig.js
@@ -2,8 +2,8 @@ module.exports = {
   name: 'Projects',
   shortName: 'Projects',
   description: 'Welcome! Please, take a look at all the projects I have been working on',
-  url: 'https://mattborghi.github.io/projects', // Domain of your site without prefix!
-  prefix: '/',
+  url: 'https://mattborghi.github.io', // Domain of your site without prefix!
+  prefix: '/projects',
   author: 'Matias Borghi',
   github: 'mattborghi',
   linkedin: 'borghimatias'
